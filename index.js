8cbd60e8df4f7c1a80f615f4f65a19871685983d
mashad <a>
2019-09-23 20:03:49 +0500

    done

--- index.js
+++ index.js
@@ -26,20 +26,102 @@ mysqlConnection.connect((err)=>{
 app.listen(4000,console.log(`listening server on port 3000`));
 
 
+const cors = require('cors');
+  var corOptions={
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+  }
+
+
+app.use(cors(corOptions))
+
+
+app.get('/power',(req,res)=>{
+  mysqlConnection.query(`SELECT * from power`,(err,rows,fields)=>{
+      if(!err)
+          res.send(rows);
+      else   
+          console.log(err);    
+  })
+});
+
+
+
+app.get('/power/:id',(req,res)=>{
+let id = req.params.id 
+mysqlConnection.query(`SELECT * from power WHERE id=${id}`,(err,rows,fields)=>{
+    if(!err)
+        res.send(rows)
+    else   
+        console.log(err);    
+})
+});
+
+//POST 
+
+app.post('/power',(req,res)=>{
+//onsole.log('req  body',req.body)
+  
+  const {id,name} = req.body
+  mysqlConnection.query(`Insert into power(name) Values('${req.body.name}') `,(err, result)=>{
+      if (err) throw err;
+      res.json(result)
+      console.log(result);
+    });
+})
+
+
+
+app.delete('/power/:id', (req, res) => {
+  let id = req.params.id
+  mysqlConnection.query(`DELETE FROM power Where id=${id}`, function (err, rows, fields) {
+    if (err)
+      res.json({ msg: err.message });;
+    console.log('deleted');
+
+  });
+})
+
+app.put('/power/:id', (req, res) => {
+  let id = req.params.id
+  const name =req.body.name;
+  mysqlConnection.query(`UPDATE power SET name = '${name}' WHERE id = '${id}'`, function (err, rows) {
+    if (err)
+      res.json({ msg: err.message });;
+    res.json(rows)
+
+  });
+})
+
+/////////////hero
 app.get('/hero',(req,res)=>{
-    mysqlConnection.query(`SELECT * from hero`,(err,rows,fields)=>{
+    mysqlConnection.query(`SELECT * from heros`,(err,rows,fields)=>{
         if(!err)
             res.send(rows);
         else   
             console.log(err);    
     })
 });
+
+
+
+app.get('/hero/:id',(req,res)=>{
+  let id = req.params.id 
+  mysqlConnection.query(`SELECT * from heros WHERE id=${id}`,(err,rows,fields)=>{
+      if(!err)
+          res.send(rows);
+      else   
+          console.log(err);    
+  })
+});
+
 //POST 
 
 app.post('/hero',(req,res)=>{
+  //console.log('req  body',req.body)
     
     const {id,name} = req.body
-    mysqlConnection.query(`Insert into hero(id,name) Values('${id}','${name}') `,(err, result)=>{
+    mysqlConnection.query(`Insert into heros(name) Values('${req.body.name}') `,(err, result)=>{
         if (err) throw err;
         res.json(result)
         console.log(result);
@@ -50,7 +132,7 @@ app.post('/hero',(req,res)=>{
 
 app.delete('/hero/:id', (req, res) => {
     let id = req.params.id
-    mysqlConnection.query(`DELETE FROM hero Where id=${id}`, function (err, rows, fields) {
+    mysqlConnection.query(`DELETE FROM heros Where id=${id}`, function (err, rows, fields) {
       if (err)
         res.json({ msg: err.message });;
       console.log('deleted');
@@ -61,10 +143,52 @@ app.delete('/hero/:id', (req, res) => {
 app.put('/hero/:id', (req, res) => {
     let id = req.params.id
     const name =req.body.name;
-    mysqlConnection.query(`UPDATE hero SET name = '${name}' WHERE id = '${id}'`, function (err, rows) {
+    mysqlConnection.query(`UPDATE heros SET name = '${name}' WHERE id = '${id}'`, function (err, rows) {
       if (err)
         res.json({ msg: err.message });;
       res.json(rows)
   
     });
   })
+  
+  /////hero powerr
+
+
+app.get('/hero/power/:id',(req,res)=>{
+  let id = req.params.id 
+  mysqlConnection.query(`SELECT p.id,p.name from heropower hp, power p 
+     WHERE hp.pid=p.id AND hid=${id}`,(err,rows,fields)=>{
+      if(!err)
+          res.send(rows);
+      else   
+          console.log(err);    
+  })
+  });
+
+app.post('/heropower/',(req,res)=>{
+  let pid = req.body.pid
+  let hid = req.body.hid
+//  console.log(pid,hid);
+  //res.json({pid,hid});
+  console.log("here is your ids",pid,hid);
+
+  mysqlConnection.query(`insert into heropower(hid,pid) VALUES('${hid}','${pid}')`,(err,rows,fields)=>{
+      if(!err)
+          res.send(rows);
+      else   
+          console.log(err);    
+  })
+}
+  );
+
+app.delete('/heropower/:id', (req, res) => {
+  let id = req.params.id
+  res.json({id});
+  //console.log("del id",id);
+   mysqlConnection.query(`DELETE FROM heropower Where pid=${id}`, function (err, rows, fields) {
+    if (err)
+      res.json({ msg: err.message });;
+    console.log('deleted');
+
+  });
+})
